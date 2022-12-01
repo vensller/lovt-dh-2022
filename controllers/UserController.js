5d714177c5b2096ee185b3953d4fe67ed1ea2f42
Ivens Diego Muller <ivensdiegomuller@gmail.com>
2022-12-01 20:33:31 -0300

    finish user crud

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -13,7 +13,48 @@ function getUserById(req, res) {
   });
 }
 
+function createUser(req, res) {
+  // Criar ou alterar - Validações são obrigatórias
+  const { name, lastName, email, password } = req.body;
+  database.User.create({
+    uname: name,
+    upassword: password,
+    lastName,
+    email,
+  }).then((data) => {
+    return res.json(data);
+  });
+}
+
+function updateUser(req, res) {
+  const { id } = req.params;
+  const { name, lastName, email, password } = req.body;
+  database.User.update(
+    {
+      uname: name,
+      upassword: password,
+      email,
+      lastName,
+    },
+    {
+      where: {
+        id,
+      },
+    }
+  ).then((data) => {
+    res.json(data);
+  });
+}
+
+function deleteUser(req, res) {
+  const { id } = req.params;
+  database.User.destroy({ where: { id } }).then((data) => res.json(data));
+}
+
 module.exports = {
   getUsers,
   getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,5 +4,13 @@ const controller = require("../controllers/UserController");
 
 router.get("/", controller.getUsers);
 router.get("/:id", controller.getUserById);
+router.post("/", controller.createUser);
+router.put("/:id", controller.updateUser);
+router.delete("/:id", controller.deleteUser);
+
+// GET = Leitura
+// POST = Criação de dados
+// PUT = Alteração de dados
+// DELETE = Remover dados
 
 module.exports = router;
