c873c159e50d7cf726020b53352a77cba9cf0a1e
Ivens Diego Muller <ivensdiegomuller@gmail.com>
2022-12-13 20:54:13 -0300

    changed user api response

---
+++ database/models/Category.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    "Category",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      Cname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "category",
+      timestamps: false,
+    }
+  );
+
+  return Category;
+};
